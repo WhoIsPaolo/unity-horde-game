8cab667cbb24a57207e066afcfce28782f918175
Paolo <paofab_98@outlook.com>
2023-07-19 12:17:40 -0500

    ...

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -112,6 +112,8 @@ public class PlayerController : MonoBehaviour
     private void Flip()
     {
         _facingRight = !_facingRight;
+
+        // Se modifica LocalScaleX para cambiar de sentido la animación del personaje
         float localScaleX = transform.localScale.x;
         localScaleX = localScaleX * -1f;
         transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
